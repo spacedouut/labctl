24d4277fa1683e3e0b203dc460bafd758d6b0908
spacedouut <spaced@spacedouut.xyz>
2026-06-27 23:40:40 -0400

    Revert "fix(phase): catch gum input exit code 124 on Ctrl+C"
    This reverts commit 82af238396c842703ec70899c5714bd151ee36e2.

--- cmds.sh
+++ cmds.sh
@@ -121,7 +121,7 @@ gather_plan() {
     if [[ -z "$bridge" ]]; then
       local -a brs=(); mapfile -t brs < <(list_bridges)
       if ((${#brs[@]})); then
-        bridge="$(gum_or_abort choose --header "Network bridge" "${brs[@]}")" || bridge=""
+        bridge="$(gum_or_abort choose --header "Network bridge" "${brs[@]}")" || true
       fi
     fi
     if [[ -z "$vlan" ]]; then
